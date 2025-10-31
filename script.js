10a93465bd959767a41920cfd35f7b5c8b57df43
ltr754731-max <ltr754731@gmail.com>
2025-10-31 21:04:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<canvas id="gameCanvas" width="400" height="600"></canvas>
+
+<script>
+    // --- 1. SETUP THE CANVAS AND CONTEXT ---
+    const canvas = document.getElementById('gameCanvas');
+    // ... all the game logic follows below ...
+</script>
